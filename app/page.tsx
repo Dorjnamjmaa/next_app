604c4e427a1f3779e66cbc92f8e92150ed844a5f
Dorjnamjmaa <124252193+Dorjnamjmaa@users.noreply.github.com>
2024-11-07 14:10:21 +0800

    dashboard app addings

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,13 @@ import Image from 'next/image';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className={styles.shape} />
-      <AcmeLogo />
+    {/* <div className={styles.shape} /> */}
+<a className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40"> <AcmeLogo /></a>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-        <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+        <p
+      className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
+    >
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
